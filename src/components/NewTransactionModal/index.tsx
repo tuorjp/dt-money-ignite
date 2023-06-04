c7f66ccf1169a931a682d3329c91fc0beae82c6d
JP <jplmarotinho22@gmail.com>
2023-06-04 16:32:34 -0300

    feat: :sparkles: radix radiogroup added

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -17,11 +17,11 @@ export function NewTransactionModal() {
             <input type="number" placeholder='Preço' required/>
             <input type="text" placeholder='Categoria' required/>
             <TransactionType>
-                <TransactionTypeButton variant='income'>
+                <TransactionTypeButton value='income' variant='income'>
                     <ArrowCircleUp size={24}/>
                     Entrada
                 </TransactionTypeButton>
-                <TransactionTypeButton variant='outcome'>
+                <TransactionTypeButton value='outcome' variant='outcome'>
                     <ArrowCircleDown size={24}/>
                     Saída
                 </TransactionTypeButton>
