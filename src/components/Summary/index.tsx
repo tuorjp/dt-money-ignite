bd2bfd43855e8990d5dc37f68850c0f0b34fd262
JP <jplmarotinho22@gmail.com>
2023-06-08 14:37:17 -0300

    summary implemented

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,7 +7,18 @@ import { TransactionsContext } from '../../contexts/TransactionsContext'
 export function Summary() {
   const { transactions } = useContext(TransactionsContext)
 
-  console.log(transactions)
+  // reduzindo o array para o objeto que queremos, acc é o objeto que definimos no segundo argumento
+  const summary = transactions.reduce((acc, transaction) => {
+    if(transaction.type === 'income'){
+      acc.income += transaction.price
+      acc.total += transaction.price
+    }
+    else {
+      acc.outcome += transaction.price
+      acc.total -= transaction.price
+    }
+    return acc
+  }, {income: 0, outcome: 0, total: 0})
 
   return (
     <SummaryContainer>
@@ -16,21 +27,21 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={32} color='#00b37e'/>
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
           <span>Saídas</span>
           <ArrowCircleDown size={32} color='#f75a68'/>
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
       <SummaryCard variant='green'>
         <header>
           <span>Total</span>
           <CurrencyDollar size={32} />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
