46dfbb4ef00311ea90947be69c210c6eb5a82fb2
JP <jplmarotinho22@gmail.com>
2023-06-16 12:06:35 -0300

    colocando lógica para dentro do contexto e otimizando método put

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -11,9 +11,17 @@ interface Transaction {
     createdAt: string
   }
 
+  interface CreateTransactionInput {
+    description: string,
+    price: number,
+    category: string,
+    type: 'income' | 'outcome'
+  }
+
 interface TransactionsContextType {
     transactions: Transaction[]
     fetchTransactions: (query?:string) => Promise<void>
+    createTransaction: (data:CreateTransactionInput) => Promise<void>
 }
 
 interface TransactionsProviderProps {
@@ -26,8 +34,12 @@ export function TransactionsProvider ({children}: TransactionsProviderProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([])
 
     async function fetchTransactions(query?:string) {
+      // a api já guarda a url base, basta passar a rota com ou sem barra e os parâmetros da busca
+      // no caso do json-server o parâmetro que vai na url se chama q
       const response = await api.get('/transactions', {
         params: {
+          _sort: 'createdAt',
+          _order: 'desc',
           q: query
         }
       })
@@ -35,6 +47,21 @@ export function TransactionsProvider ({children}: TransactionsProviderProps) {
         setTransactions(response.data)
       }
     
+      async function createTransaction(data: CreateTransactionInput) {
+    // basta passar a rota e o corpo da requisição
+        const {description, price, category, type} = data
+        
+        const response = await api.post('transactions', {
+          description,
+          price,
+          category,
+          type,
+          createdAt: new Date(),
+        })
+
+        setTransactions(state => [response.data, ...state])
+      }
+
       useEffect(() => {
         fetchTransactions()
       }, [])
@@ -46,7 +73,7 @@ export function TransactionsProvider ({children}: TransactionsProviderProps) {
       // }, [])
       
     return(
-        <TransactionsContext.Provider value={{transactions, fetchTransactions}}>
+        <TransactionsContext.Provider value={{transactions, fetchTransactions, createTransaction}}>
             {children}
         </TransactionsContext.Provider>
     )
