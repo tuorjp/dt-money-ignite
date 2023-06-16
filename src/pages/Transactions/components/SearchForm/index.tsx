4c902a83a807f9334cee2a0fc15fdbb3bbfa4f9b
JP <jplmarotinho22@gmail.com>
2023-06-16 11:40:41 -0300

    buscando transações

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -4,6 +4,8 @@ import { SearchFormContainer } from "./styles";
 import { useForm } from 'react-hook-form';
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useContext } from 'react';
+import { TransactionsContext } from '../../../../contexts/TransactionsContext';
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -12,13 +14,14 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const {fetchTransactions} = useContext(TransactionsContext)
+
   const {register, handleSubmit, formState: {isSubmitting}} = useForm<SearchFormInputs>({
     resolver: zodResolver(searchFormSchema)
   })
 
   async function handleSearchTransaction(data: SearchFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 2000))
-    console.log(data)
+    await fetchTransactions(data.query)
   }
 
   return (
