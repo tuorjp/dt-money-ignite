8f130cf47c71932067263daa069d74570fa409e0
JP <jplmarotinho22@gmail.com>
2023-06-16 11:13:41 -0300

    zod, schema validation and react hook form added

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,14 +1,35 @@
 /* eslint-disable prettier/prettier */
 import {MagnifyingGlass} from 'phosphor-react'
 import { SearchFormContainer } from "./styles";
+import { useForm } from 'react-hook-form';
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod';
 
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-  return (
-    <SearchFormContainer>
-        <input type="text" placeholder="Busque por transações" />
+  const {register, handleSubmit, formState: {isSubmitting}} = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema)
+  })
 
-        <button type="submit">
+  async function handleSearchTransaction(data: SearchFormInputs) {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    console.log(data)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransaction)}>
+        <input
+          type="text"
+          placeholder="Busque por transações"
+          {...register('query')}
+        />
+
+        <button type="submit" disabled={isSubmitting}>
             <MagnifyingGlass size={20}/>
             Buscar
         </button>
