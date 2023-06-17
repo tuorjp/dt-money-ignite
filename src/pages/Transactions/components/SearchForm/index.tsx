a27a4ba6ad8eece874bb72718c2525baff5ee661
JP <jplmarotinho22@gmail.com>
2023-06-17 19:23:17 -0300

    aplicando o memo para evitar re-render

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,11 +1,29 @@
 /* eslint-disable prettier/prettier */
 import {MagnifyingGlass} from 'phosphor-react'
-import { SearchFormContainer } from "./styles";
-import { useForm } from 'react-hook-form';
+import { SearchFormContainer } from "./styles"
+import { useForm } from 'react-hook-form'
 import * as z from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod';
-import { TransactionsContext } from '../../../../contexts/TransactionsContext';
-import { useContextSelector } from 'use-context-selector';
+import { zodResolver } from '@hookform/resolvers/zod'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
+import { useContextSelector } from 'use-context-selector'
+import { memo } from 'react'
+
+/*
+* Por que um componente renderiza novamente?
+* Hooks changed (mudou estado, contexto, reducer)
+* Props changed (propriedades mudaram)
+* Parent rendered (componente pai renderizou)
+
+O fluxo de renderização:
+1 Recriação do html (interface) do componente
+2 Compara a versão recriada com a versão anterior
+3 Se mudou algo, reescreve o html na tela
+
+O memo é usado para memorizar um componente:
+0 Antes do fluxo de renderização: Hooks changed, Props changed ? (deep comparison)
+01 Compara com a versão anterior dos hooks e props
+02 Se mudou algo, cai para o fluxo de recriação
+*/
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -13,7 +31,7 @@ const searchFormSchema = z.object({
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
-export function SearchForm() {
+function SearchFormComponent() {
   const fetchTransactions = useContextSelector(TransactionsContext, (context) => {
     return context.fetchTransactions
   })
@@ -41,3 +59,5 @@ export function SearchForm() {
     </SearchFormContainer>
   )
 }
+
+export const SearchForm = memo(SearchFormComponent)
